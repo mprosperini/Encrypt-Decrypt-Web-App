19efbd5aaa3660d3904e7cc8012fd3063758ebd5
mprosperini <maupc9@gmail.com>
2023-01-25 20:00:39 -0400

    Se agrega funcionalidad al boton copiar

--- js/java.js
+++ js/java.js
@@ -1,3 +1,5 @@
+// ---- ---- ---- ---- ---- ---- ---- ---- DARK/LIGHT MODE ---- ---- ---- ---- ---- ----- ---- ----//
+
 const lightModeBtn = document.querySelector("#sun-icon");
 const darkModeBtn = document.querySelector("#moon-icon");
 
@@ -15,3 +17,20 @@ function setLightMode () {
 function setUserTheme(newTheme) {
     document.documentElement.setAttribute("data-theme",newTheme);
 }
+
+// ---- ---- ---- ---- ---- ---- ---- ---- COPY BUTTON + PROMPT ---- ---- ---- ---- ---- ----- ---- ----//
+
+let copyButton = document.getElementById("copy-button");
+let encryptedResult = document.getElementById("encrypted-result");
+
+copyButton.addEventListener("click",function() {
+    let selection = window.getSelection();
+    selection.selectAllChildren(encryptedResult);
+    document.execCommand("copy");
+
+    document.getElementById("copied-prompt").style.opacity="90%";
+
+    setTimeout(() => {
+        document.getElementById("copied-prompt").style.opacity="0%";
+    }, 1000);
+})
