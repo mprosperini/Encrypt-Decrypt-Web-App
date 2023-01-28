12a49748f97dd7a25462886dc07148d03d705f9b
mprosperini <maupc9@gmail.com>
2023-01-27 22:39:12 -0400

    se agrega la logica para que funcionen los botones de encriptar y desencriptar

--- js/java.js
+++ js/java.js
@@ -1,5 +1,4 @@
 // ---- ---- ---- ---- ---- ---- ---- ---- DARK/LIGHT MODE ---- ---- ---- ---- ---- ----- ---- ----//
-
 const lightModeBtn = document.querySelector("#sun-icon");
 const darkModeBtn = document.querySelector("#moon-icon");
 
@@ -18,8 +17,37 @@ function setUserTheme(newTheme) {
     document.documentElement.setAttribute("data-theme",newTheme);
 }
 
-// ---- ---- ---- ---- ---- ---- ---- ---- COPY BUTTON + PROMPT ---- ---- ---- ---- ---- ----- ---- ----//
+// ---- ---- ---- ---- ---- ---- ---- ---- ENCRYPT BUTTON ---- ---- ---- ---- ---- ----- ---- ----//
+let encryptKeys = [["a","ai"],["e","enter"],["i","imes"],["o","ober"],["u","ufat"]]
+const encrypBtn = document.getElementById("encrypt-btn");
+let insertedText = document.getElementById("input-area");
 
+encrypBtn.addEventListener("click",function(){
+
+    insertedText.value = insertedText.value.toLowerCase();
+    
+    for (let i = 0; i < encryptKeys.length; i++) {
+        if (insertedText.value.includes(encryptKeys[i][0])) {
+            encryptedResult.textContent = insertedText.value.replaceAll(encryptKeys[i][0],encryptKeys[i][1]);
+        }        
+    }
+    
+    insertedText.value = "";
+})
+
+// ---- ---- ---- ---- ---- ---- ---- ---- DECRYPT BUTTON ---- ---- ---- ---- ---- ----- ---- ----//
+const decryptBtn = document.getElementById("decrypt-btn");
+
+decryptBtn.addEventListener("click",function(){
+
+    for (let i = 0; i  < encryptKeys.length; i++) {
+        if (encryptedResult.textContent.includes(encryptKeys[i][1])) {
+            insertedText.value = encryptedResult.textContent.replaceAll(encryptKeys[i][1],encryptKeys[i][0]);
+        }
+    }
+})
+
+// ---- ---- ---- ---- ---- ---- ---- ---- COPY BUTTON + PROMPT ---- ---- ---- ---- ---- ----- ---- ----//
 let copyButton = document.getElementById("copy-button");
 let encryptedResult = document.getElementById("encrypted-result");
 
@@ -34,3 +62,5 @@ copyButton.addEventListener("click",function() {
         document.getElementById("copied-prompt").style.opacity="0%";
     }, 1000);
 })
+
+
